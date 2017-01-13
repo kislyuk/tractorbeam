acbcc250362e2574fc99ffe46429e99589ee8c41
Andrey Kislyuk <kislyuk@gmail.com>
2017-01-13 14:59:13 -0800

    v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tractorbeam",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/kislyuk/tractorbeam",
     license="Apache Software License",
     author="Andrey Kislyuk",
